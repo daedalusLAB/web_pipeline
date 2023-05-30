9f75cee621b5b2fd8e063766fdcc2dd4b59d42d8
Raul Sanchez <raul@um.es>
2023-05-30 19:19:05 +0100

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'welcome#index'
+
+
+    
+# Allows us to use link_to for session destroy
+  devise_scope :user do
+    get "/users/sign_out", as: "sign_out", to: "devise/sessions#destroy"
+  end
+
+  
 end
