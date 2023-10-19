be6c381e858346ace286a1e05a8a972d0011bc79
Raul Sanchez <raul@um.es>
2023-10-19 13:06:15 +0200

    check name only can contains letters, numbers and spaces

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,8 @@ class Video < ApplicationRecord
     validates :zip, presence: true
     belongs_to :user
 
+    # validate that name only contains letters, numbers, and spaces. If error, display message "Name can only contain letters, numbers, and spaces."
+    #validates_format_of :name, :with => /\A[a-zA-Z0-9 ]+\z/ 
+    validates_format_of :name, :with => /\A[a-zA-Z0-9 ]+\z/, :message => "can only contain letters, numbers, and spaces."
+
 end
