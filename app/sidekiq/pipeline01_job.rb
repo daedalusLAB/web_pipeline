2490f4f54b915b489a45b4a01ead7d88eaf70027
Raul Sanchez <raul@um.es>
2024-03-07 12:19:12 +0100

    Refactor video processing pipeline and add HPC integration

--- app/sidekiq/pipeline01_job.rb
+++ app/sidekiq/pipeline01_job.rb
@@ -6,30 +6,29 @@ class Pipeline01Job
     # update the status of the video
     # send an email to the user
 
+  # get HPC credentials
+  hpc_user = ENV['HPC_USER']
+  hpc_host = ENV['HPC_HOST']
+  hpc_key = ENV['HPC_KEY']
+  
     # get the video id
     video_id = args[0]
 
     # get the video
     video = Video.find(video_id)
     # update the status of the video
-    video.status = "Processing"
-    video.save
-    PipelineMailer.with(user: video.user, status: video.status).status_email.deliver_now
 
     # exec pipeline script in the rails app bin folder to process the videos
     puts "**********************************************************************"
     puts "bin/pipeline.sh #{video.zip.file.path}  \"#{video.name}\" "
+
     dir_path = File.dirname(video.zip.file.path)
     log_file = "#{dir_path}/output_log.txt"
     # get the output of the script and print it to the console. If there is an error, it will be printed to the console
-    error = system("bin/pipeline.sh #{video.zip.file.path}  \"#{video.name}\" > #{log_file} 2>&1 ")
-    puts "**********************************************************************"
-    puts "ERROR: #{error}"
-    puts "**********************************************************************"
-
-    if error
+    resultOK = system("bin/pipeline.sh #{video.zip.file.path}  \"#{video.name}\" \"#{video.id}\" \"#{hpc_user}\" \"#{hpc_host}\" \"#{hpc_key}\" > #{log_file} 2>&1 ")
+    if resultOK
     # update the status of the video
-      video.status = "Processed"
+    video.status = "Processing"
     else
       video.status = "Error"
     end
