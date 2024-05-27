3098b6158d789d779d2cf160fdab8df480be075a
Raul Sanchez <raul@um.es>
2024-05-27 09:52:06 +0200

    chore: Update form control focus styling in devise views and post ordering in welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   
   def index
-    # get all post with published top true
+    # get all post with published top true order by order
     @posts = Post.where(published: true)
+                  .order(order: :asc)
   end
 end
